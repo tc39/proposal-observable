039ebb385641136c18ad95371f414a05207f4051
zenparsing <zenparsing@gmail.com>
2015-05-05 22:24:42 -0400

    Revert return type of observable init to a cleanup function.

--- Observable.js
+++ Observable.js
@@ -2,23 +2,23 @@ class ObserverSink {
 
     constructor(observer) {
 
-        this.done = false;
-        this.controller = undefined;
-        this.observer = observer;
+        this._done = false;
+        this._cleanup = undefined;
+        this._observer = observer;
     }
 
-    close() {
+    _close() {
 
-        this.done = true;
+        this._done = true;
 
-        if (this.controller && "stop" in this.controller)
-            this.controller.stop();
+        if (this._cleanup)
+            this._cleanup.call(undefined);
     }
 
     next(value) {
 
         // If the stream if closed, then return a "done" result
-        if (this.done)
+        if (this._done)
             return { done: true };
 
         let result;
@@ -26,18 +26,18 @@ class ObserverSink {
         try {
 
             // Send the next value to the sink
-            result = this.observer.next(value);
+            result = this._observer.next(value);
 
         } catch (e) {
 
             // If the observer throws, then close the stream and rethrow the error
-            this.close();
+            this._close();
             throw e;
         }
 
         // Cleanup if sink is closed
-        if (result && result.done)
-            this.close();
+        if (result && result._done)
+            this._close();
 
         return result;
     }
@@ -45,44 +45,44 @@ class ObserverSink {
     throw(value) {
 
         // If the stream is closed, throw the error to the caller
-        if (this.done)
+        if (this._done)
             throw value;
 
-        this.done = true;
+        this._done = true;
 
         try {
 
             // If the sink does not support "throw", then throw the error to the caller
-            if (!("throw" in this.observer))
+            if (!("throw" in this._observer))
                 throw value;
 
-            return this.observer.throw(value);
+            return this._observer.throw(value);
 
         } finally {
 
-            this.close();
+            this._close();
         }
     }
 
     return(value) {
 
         // If the stream is closed, then return a done result
-        if (this.done)
+        if (this._done)
             return { done: true };
 
-        this.done = true;
+        this._done = true;
 
         try {
 
             // If the sink does not support "return", then return a done result
-            if (!("return" in this.observer))
+            if (!("return" in this._observer))
                 return { done: true };
 
-            return this.observer.return(value);
+            return this._observer.return(value);
 
         } finally {
 
-            this.close();
+            this._close();
         }
     }
 }
@@ -105,24 +105,16 @@ class Observable {
             throw new TypeError("Observer must be an object");
 
         let sink = new ObserverSink(observer),
-            controller;
+            cleanup;
 
         try {
 
-            // Call the stream initializer.  The initializer will return a
-            // stream controller or undefined.
-            controller = this._init.call(undefined, sink);
+            // Call the stream initializer
+            cleanup = this._init.call(undefined, sink);
 
-            // The returned controller may be null or undefined
-            if (controller == null)
-                controller = {};
-
-            // The controller must be an object
-            if (Object(controller) !== controller)
-                throw new TypeError("Stream controller must be an object");
-
-            if ("start" in controller)
-                controller.start();
+            // The return value must be a function or null or undefined
+            if (cleanup != null && typeof cleanup !== "function")
+                throw new TypeError(cleanup + " is not a function");
 
         } catch (e) {
 
@@ -133,17 +125,13 @@ class Observable {
         }
 
         // If the stream is already finished, then perform cleanup
-        if (sink.done && "stop" in controller)
-            controller.stop();
+        if (sink._done && cleanup)
+            cleanup.call(undefined);
 
-        sink.controller = controller;
+        sink._cleanup = cleanup;
 
         // Return a cancellation function
-        return _=> {
-
-            if ("cancel" in controller) controller.cancel();
-            else sink.return();
-        };
+        return _=> { sink.return() };
     }
 
     forEach(fn) {
@@ -164,22 +152,18 @@ class Observable {
         if (typeof fn !== "function")
             throw new TypeError(fn + " is not a function");
 
-        return new this.constructor(sink => ({
+        return new this.constructor[Symbol.species](sink => this.subscribe({
 
-            stop: this.subscribe({
+            next(value) {
 
-                next(value) {
+                try { value = fn(value) }
+                catch (e) { return sink.throw(e) }
 
-                    try { value = fn(value) }
-                    catch (e) { return sink.throw(e) }
+                return sink.next(value);
+            },
 
-                    return sink.next(value);
-                },
-
-                throw(value) { return sink.throw(value) },
-                return(value) { return sink.return(value) },
-            })
-
+            throw(value) { return sink.throw(value) },
+            return(value) { return sink.return(value) },
         }));
     }
 
@@ -188,23 +172,23 @@ class Observable {
         if (typeof fn !== "function")
             throw new TypeError(fn + " is not a function");
 
-        return new this.constructor(sink => ({
+        return new this.constructor[Symbol.species](sink => this.subscribe({
 
-            stop: this.subscribe({
+            next(value) {
 
-                next(value) {
+                try { if (!fn(value)) return { done: false } }
+                catch (e) { return sink.throw(e) }
 
-                    try { if (!fn(value)) return { done: false } }
-                    catch (e) { return sink.throw(e) }
+                return sink.next(value);
+            },
 
-                    return sink.next(value);
-                },
-
-                throw(value) { return sink.throw(value) },
-                return(value) { return sink.return(value) },
-            })
-
+            throw(value) { return sink.throw(value) },
+            return(value) { return sink.return(value) },
         }));
     }
+
+    [Symbol.observable]() { return this }
+
+    static get [Symbol.species]() { return this }
 
 }
