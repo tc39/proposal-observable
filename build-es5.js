661ee3446123e8deff476e01c3bcf72e12aeb794
zenparsing <zenparsing@gmail.com>
2015-08-17 15:23:44 -0400

    Update built es5 files.

--- build-es5.js
+++ build-es5.js
@@ -2,15 +2,19 @@ var ChildProcess = require("child_process");
 
 var args = "- ./src/Observable.js ../build/esdown.js -b -r -p -g esdown";
 
-var child = ChildProcess.spawn(
-    "esdown",
-    "- ./polyfill.js ./es5/observable-polyfill.js -r".split(/ /g),
-    { stdio: "inherit", env: process.env, cwd: __dirname });
+function compile(args) {
 
-child.on("close", function() {
+    return new Promise(function(resolve) {
 
-    ChildProcess.spawn(
-        "esdown",
-        "- ./test/default.js ./es5/observable-tests.js -b -r -g ObservableTests".split(/ /g),
-        { stdio: "inherit", env: process.env, cwd: __dirname });
+        var child = ChildProcess.spawn(
+            "esdown",
+            ("- " + args).split(/ /g),
+            { stdio: "inherit", env: process.env, cwd: __dirname });
+
+        child.on("close", resolve);
+    });
+}
+
+compile("./polyfill.js ./es5/observable-polyfill.js -b -r").then(function() {
+    return compile("./test/default.js ./es5/observable-tests.js -b -r -g ObservableTests");
 });
--- es5/observable-polyfill.js
+++ es5/observable-polyfill.js
@@ -1,4 +1,4 @@
-/*=esdown=*/(function(fn, deps, name) { function obj() { return {} } if (typeof exports !== 'undefined') fn(require, exports, module); else if (typeof define === 'function' && define.amd) define(['require', 'exports', 'module'].concat(deps), fn); else if (typeof window !== 'undefined' && name) fn(obj, window[name] = {}, {}); else fn(obj, {}, {}); })(function(require, exports, module) { 'use strict'; function __load(p, l) { module.__es6 = !l; var e = require(p); if (e && e.constructor !== Object) e.default = e; return e; } var _M0 = __load("./src/Observable.js"); 
+/*=esdown=*/(function(fn, deps, name) { function obj() { return {} } if (typeof exports !== 'undefined') fn(require, exports, module); else if (typeof define === 'function' && define.amd) define(['require', 'exports', 'module'].concat(deps), fn); else if (typeof window !== 'undefined' && name) fn(obj, window[name] = {}, {}); else fn(obj, {}, {}); })(function(require, exports, module) { 'use strict'; function __load(p, l) { module.__es6 = !l; var e = require(p); if (e && e.constructor !== Object) e.default = e; return e; } 
 var _esdown; (function() {
 
 var VERSION = "0.9.11";
@@ -396,7 +396,426 @@ _esdown = {
 
 }).call(this);
 
-var Observable = _M0.Observable;
+
+
+var _M2 = {}, _M1 = exports;
+
+(function(exports) {
+
+// === Non-Promise Job Queueing ===
+
+var enqueueJob = (function() {
+
+    // Node
+    if (typeof global !== "undefined" &&
+        typeof process !== "undefined" &&
+        process.nextTick) {
+
+        return global.setImmediate ?
+            function(fn) { global.setImmediate(fn) } :
+            function(fn) { process.nextTick(fn) };
+    }
+
+    // Newish Browsers
+    var Observer = self.MutationObserver || self.WebKitMutationObserver;
+
+    if (Observer) {
+
+        var div$0 = document.createElement("div"),
+            twiddle$0 = function(_) { return div$0.classList.toggle("x"); },
+            queue$0 = [];
+
+        var observer$0 = new Observer(function(_) {
+
+            if (queue$0.length > 1)
+                twiddle$0();
+
+            while (queue$0.length > 0)
+                queue$0.shift()();
+        });
+
+        observer$0.observe(div$0, { attributes: true });
+
+        return function(fn) {
+
+            queue$0.push(fn);
+
+            if (queue$0.length === 1)
+                twiddle$0();
+        };
+    }
+
+    // Fallback
+    return function(fn) { setTimeout(fn, 0) };
+
+})();
+
+// === Symbol Polyfills ===
+
+function polyfillSymbol(name) {
+
+    if (!Symbol[name])
+        Object.defineProperty(Symbol, name, { value: Symbol(name) });
+}
+
+polyfillSymbol("observable");
+
+// === Abstract Operations ===
+
+function getMethod(obj, key) {
+
+    var value = obj[key];
+
+    if (value == null)
+        return undefined;
+
+    if (typeof value !== "function")
+        throw new TypeError(value + " is not a function");
+
+    return value;
+}
+
+function cleanupSubscription(observer) {
+
+    // Assert:  observer._observer is undefined
+
+    var cleanup = observer._cleanup;
+
+    if (!cleanup)
+        return;
+
+    // Drop the reference to the cleanup function so that we won't call it
+    // more than once
+    observer._cleanup = undefined;
+
+    // Call the cleanup function
+    cleanup();
+}
+
+function subscriptionClosed(observer) {
+
+    return observer._observer === undefined;
+}
+
+var SubscriptionObserver = _esdown.class(function(__) { var SubscriptionObserver;
+
+    __({ constructor: SubscriptionObserver = function(observer, subscriber) {
+
+        // Assert: subscriber is callable
+
+        // The observer must be an object
+        if (Object(observer) !== observer)
+            throw new TypeError("Observer must be an object");
+
+        this._observer = observer;
+        this._cleanup = undefined;
+
+        try {
+
+            // Call the subscriber function
+            var cleanup$0 = subscriber.call(undefined, this);
+
+            // The return value must be undefined, null, or a function
+            if (cleanup$0 != null && typeof cleanup$0 !== "function")
+                throw new TypeError(cleanup$0 + " is not a function");
+
+            this._cleanup = cleanup$0;
+
+        } catch (e) {
+
+            // If an error occurs during startup, then attempt to send the error
+            // to the observer
+            this.error(e);
+            return;
+        }
+
+        // If the stream is already finished, then perform cleanup
+        if (subscriptionClosed(this))
+            cleanupSubscription(this);
+    },
+
+    cancel: function() {
+
+        if (subscriptionClosed(this))
+            return;
+
+        this._observer = undefined;
+        cleanupSubscription(this);
+    },
+
+    get closed() { return subscriptionClosed(this) },
+
+    next: function(value) {
+
+        // If the stream if closed, then return undefined
+        if (subscriptionClosed(this))
+            return undefined;
+
+        var observer = this._observer;
+
+        try {
+
+            var m$0 = getMethod(observer, "next");
+
+            // If the observer doesn't support "next", then return undefined
+            if (!m$0)
+                return undefined;
+
+            // Send the next value to the sink
+            return m$0.call(observer, value);
+
+        } catch (e) {
+
+            // If the observer throws, then close the stream and rethrow the error
+            try { this.cancel() }
+            finally { throw e }
+        }
+    },
+
+    error: function(value) {
+
+        // If the stream is closed, throw the error to the caller
+        if (subscriptionClosed(this))
+            throw value;
+
+        var observer = this._observer;
+        this._observer = undefined;
+
+        try {
+
+            var m$1 = getMethod(observer, "error");
+
+            // If the sink does not support "error", then throw the error to the caller
+            if (!m$1)
+                throw value;
+
+            value = m$1.call(observer, value);
+
+        } catch (e) {
+
+            try { cleanupSubscription(this) }
+            finally { throw e }
+        }
+
+        cleanupSubscription(this);
+
+        return value;
+    },
+
+    complete: function(value) {
+
+        // If the stream is closed, then return undefined
+        if (subscriptionClosed(this))
+            return undefined;
+
+        var observer = this._observer;
+        this._observer = undefined;
+
+        try {
+
+            var m$2 = getMethod(observer, "complete");
+
+            // If the sink does not support "complete", then return undefined
+            value = m$2 ? m$2.call(observer, value) : undefined;
+
+        } catch (e) {
+
+            try { cleanupSubscription(this) }
+            finally { throw e }
+        }
+
+        cleanupSubscription(this);
+
+        return value;
+    }});
+
+ });
+
+// SubscriptionObserver instances should report Object as their constructor
+// (see ArrayIterator and MapIterator).  Ideally, the prototype should not
+// have an own constructor property, but for this polyfill we want to avoid
+// using the delete operator.
+SubscriptionObserver.prototype.constructor = Object;
+
+var Observable = _esdown.class(function(__) { var Observable;
+
+    // == Fundamental ==
+
+    __({ constructor: Observable = function(subscriber) {
+
+        // The stream subscriber must be a function
+        if (typeof subscriber !== "function")
+            throw new TypeError("Observable initializer must be a function");
+
+        this._subscriber = subscriber;
+    },
+
+    subscribe: function(observer) {
+
+        // Wrap the observer in order to maintain observation invariants
+        observer = new SubscriptionObserver(observer, this._subscriber);
+        return function(_) { observer.cancel() };
+    },
+
+    forEach: function(fn) { var __this = this; 
+
+        return new Promise(function(resolve, reject) {
+
+            if (typeof fn !== "function")
+                throw new TypeError(fn + " is not a function");
+
+            __this.subscribe({
+
+                next: function(value) {
+
+                    try { return fn(value) }
+                    catch (x) { reject(x) }
+                },
+
+                error: reject,
+                complete: resolve,
+            });
+        });
+    }});
+
+    __(_esdown.computed({}, Symbol.observable, { _: function() { return this } }));
+
+    __.static(_esdown.computed({}, Symbol.species, { get _() { return this } }));
+
+    // == Derived ==
+
+    __.static({ from: function(x) {
+
+        var C = typeof this === "function" ? this : Observable;
+
+        if (x == null)
+            throw new TypeError(x + " is not an object");
+
+        var method = getMethod(x, Symbol.observable);
+
+        if (method) {
+
+            var observable$0 = method.call(x);
+
+            if (Object(observable$0) !== observable$0)
+                throw new TypeError(observable$0 + " is not an object");
+
+            if (observable$0.constructor === C)
+                return observable$0;
+
+            return new C(function(observer) { return observable$0.subscribe(observer); });
+        }
+
+        return new C(function(observer) {
+
+            enqueueJob(function(_) {
+
+                if (observer.closed)
+                    return;
+
+                // Assume that the object is iterable.  If not, then the observer
+                // will receive an error.
+                try {
+
+                    for (var __$0 = (x)[Symbol.iterator](), __$1; __$1 = __$0.next(), !__$1.done;) { var item$0 = __$1.value; 
+
+                        observer.next(item$0);
+
+                        if (observer.closed)
+                            return;
+                    }
+
+                } catch (x) {
+
+                    // If observer.next throws an error, then the subscription will
+                    // be closed and the error method will simply rethrow
+                    observer.error(x);
+                    return;
+                }
+
+                observer.complete();
+            });
+        });
+    },
+
+    of: function() { for (var items = [], __$0 = 0; __$0 < arguments.length; ++__$0) items.push(arguments[__$0]); 
+
+        var C = typeof this === "function" ? this : Observable;
+
+        return new C(function(observer) {
+
+            enqueueJob(function(_) {
+
+                if (observer.closed)
+                    return;
+
+                for (var i$0 = 0; i$0 < items.length; ++i$0) {
+
+                    observer.next(items[i$0]);
+
+                    if (observer.closed)
+                        return;
+                }
+
+                observer.complete();
+            });
+        });
+    }});
+
+    __({ map: function(fn) { var __this = this; 
+
+        if (typeof fn !== "function")
+            throw new TypeError(fn + " is not a function");
+
+        var C = this.constructor[Symbol.species];
+
+        return new C(function(observer) { return __this.subscribe({
+
+            next: function(value) {
+
+                try { value = fn(value) }
+                catch (e) { return observer.error(e) }
+
+                return observer.next(value);
+            },
+
+            error: function(value) { return observer.error(value) },
+            complete: function(value) { return observer.complete(value) },
+        }); });
+    },
+
+    filter: function(fn) { var __this = this; 
+
+        if (typeof fn !== "function")
+            throw new TypeError(fn + " is not a function");
+
+        var C = this.constructor[Symbol.species];
+
+        return new C(function(observer) { return __this.subscribe({
+
+            next: function(value) {
+
+                try { if (!fn(value)) return undefined; }
+                catch (e) { return observer.error(e) }
+
+                return observer.next(value);
+            },
+
+            error: function(value) { return observer.error(value) },
+            complete: function(value) { return observer.complete(value) },
+        }); });
+    }});
+
+ });
+
+exports.Observable = Observable;
+
+
+}).call(this, _M2);
+
+(function(exports) {
+
+var Observable = _M2.Observable;
 
 var G = (function(_) {
     try { return self.self } catch (e) {}
@@ -412,4 +831,7 @@ try {
 } catch (e) { G.Observable = Observable }
 
 
-}, ["./src/Observable.js"], "");
+}).call(this, _M1);
+
+
+}, [], "");
